aba513dea0251fc91d1cfd9068749e01e484b905
Artem Reva <artemreva79@gmail.com>
2026-05-06 17:48:22 +0300

    fix run_back.sh

--- run_back.sh
+++ run_back.sh
@@ -3,4 +3,4 @@
 python -m venv .venv
 source .venv/bin/activate
 pip install fastapi uvicorn langgraph
-uvicorn api/api.main:app --reload --host 0.0.0.0 --port 8000
+uvicorn api.main:app --reload --host 0.0.0.0 --port 8000
